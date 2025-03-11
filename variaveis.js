2502dc119033133226e88e09043916ba8dc31d93
Vitor Augusto de Almeida Buarque - 824148704 <824148704@ulife.com.br>
2025-03-11 10:12:58 -0300

    feat: Trabalhando variaveis

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+var idade = 39;
+let nome = "vitor";
+let ativo = true;
+const PI = 3.14;
+
+let num = Number("42")
+let flost = parseFloat("5.7")
+let interiro = parseFloat("10.5")
+
+console.log(num)
+console.log(flost);
+console.log(interiro);
+
+
+/*console.log(nome);
+console.log(ativo);
+console.log(PI);
+
+if (true) {
+    PI = 4.14
+    console.log(PI);
+
+}*/
