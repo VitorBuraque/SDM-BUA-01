1e5b968a8863fc163eb2c84119c5aae7ebdd6659
Vitor Augusto de Almeida Buarque - 824148704 <824148704@ulife.com.br>
2025-03-11 11:44:12 -0300

    Feat: Trabalhando com coleções

---
+++ colecoes.js
@@ -0,0 +1,32 @@
+/*let numeros = [10, 20, 30, 40, 50]
+console.log(numeros[2])
+
+let frutas = ["Maçâ", "Banana", "Pera", "Laranja"]
+console.log(frutas)
+frutas.forEach((fruta, index) => {
+    console.log(`${index}: ${fruta}`)
+})
+
+
+//adicionando elementos
+
+frutas.push("Uva")
+frutas.unshift("Melancia")
+console.log(frutas)
+
+//removendo elementos
+
+frutas.pop("uva")
+
+frutas.splice(2, 1)
+console.log(frutas)*/
+
+let mapa = new Map()
+mapa.set("nome", "Joana")
+mapa.set(42, "Idade", )
+
+
+//Acessado valores
+
+console.log(mapa.get("nome"))
+console.log(mapa.get("idade"))
